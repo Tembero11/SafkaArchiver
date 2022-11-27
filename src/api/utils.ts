a2f6478c7b505cbb3475e7a9ce3fc4d714e65f17
Tembero11 <silver.teemu@gmail.com>
2022-11-27 13:33:36 +0200

    More functional code

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -2,20 +2,31 @@ import assert from "assert"
 
 export enum QueryType {
     String = "string",
-    Integer = "integer",
+    Date = "date",
+    Float = "float",
+    NegativeFloat = "negativeFloat",
+    PositiveFloat = "positiveFloat",
+    Int = "int",
+    NegativeInt = "negativeInt",
+    PositiveInt = "positiveInt",
     Boolean = "boolean",
 }
 
 interface IQueryType {
     string: string
     boolean: boolean
-    integer: number
+    int: number
+    negativeInt: number
+    positiveInt: number
+    date: Date
 }
 
 interface QueryTemplate {
     [name: string]: keyof IQueryType
 }
-
+interface UnknownQuery {
+    [key: string]: unknown
+}
 type TypedQuery<Type extends QueryTemplate> = {
     [Property in keyof Type]: IQueryType[Type[Property]]
 }
@@ -23,35 +34,41 @@ type TypedQuery<Type extends QueryTemplate> = {
 type ParseQueryResult<A extends QueryTemplate, B extends QueryTemplate> = TypedQuery<A> & (Partial<TypedQuery<B>>);
 
 export function parseQuery<A extends QueryTemplate, B extends QueryTemplate>(
-    query: {[key: string]: unknown},
+    query: UnknownQuery,
     required: A,
     optional: B
 ): ParseQueryResult<A, B> {
-    const requiredMapped = Object.entries(required).map(queryTemplateMapper(false)); 
-    const optionalMapped = Object.entries(required).map(queryTemplateMapper(true)); 
-
-    const templateParams = [...requiredMapped, ...optionalMapped];
-
-    for (const templateParam of templateParams) {
-        const exists = Object.hasOwn(query, templateParam.name);
-        if (!templateParam.isOptional) {
-            assert(exists);
-        }
-
-        const value = query[templateParam.name];
-
-        switch (templateParam.type) {
-            case QueryType.Integer:
-                parseInteger(value);
-                break;
-        
-            default:
-                break;
-        }
+    for (const template of Object.entries(required)) {
+        const exists = Object.hasOwn(query, template[0]);
+        assert(exists);
+        checkAgainstTemplate(query[template[0]], ...template);
     }
-
     // FOR TESTING
     return "" as unknown as any;
+}
+
+function checkAgainstTemplate(queryValue: unknown, name: string, type: keyof IQueryType) {
+       switch (type) {
+        case QueryType.Boolean:
+            return parseBoolean(queryValue)
+        case QueryType.Int:
+            return parseInteger(queryValue)
+       
+        default:
+            return queryValue as string
+       }
+}
+
+function parseBoolean(value: unknown) {
+    if (typeof value != "string") throw new Error("");
+    
+    if (value == "true") {
+        return true;
+    }
+    if (value == "false") {
+        return false;
+    }
+    throw new Error("");
 }
 
 function parseInteger(value: unknown) {
@@ -63,10 +80,4 @@ function parseInteger(value: unknown) {
     assert(isInteger);
 
     return num;
-}
-
-function queryTemplateMapper(isOptional: boolean) {
-    return function([name, type]: [string, keyof IQueryType]) {
-        return { name, type, isOptional }   
-    }
 }
