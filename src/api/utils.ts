396553b39d99752289df6908d0b5b81faac6ca66
Tembero11 <silver.teemu@gmail.com>
2022-11-27 02:50:04 +0200

    Proof of concept

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,33 +1,72 @@
+import assert from "assert"
+
 export enum QueryType {
     String = "string",
-    Number = "number",
+    Integer = "integer",
     Boolean = "boolean",
 }
 
 interface IQueryType {
     string: string
     boolean: boolean
-    number: number
+    integer: number
 }
 
-interface QuerySchema {
+interface QueryTemplate {
     [name: string]: keyof IQueryType
 }
 
-type TypedQuery<Type extends QuerySchema> = {
+type TypedQuery<Type extends QueryTemplate> = {
     [Property in keyof Type]: IQueryType[Type[Property]]
 }
 
-type ParseQueryResult<A extends QuerySchema, B extends QuerySchema> = TypedQuery<A> & (Partial<TypedQuery<B>>);
+type ParseQueryResult<A extends QueryTemplate, B extends QueryTemplate> = TypedQuery<A> & (Partial<TypedQuery<B>>);
 
-export function parseQuery<A extends QuerySchema, B extends QuerySchema>(
+export function parseQuery<A extends QueryTemplate, B extends QueryTemplate>(
     query: {[key: string]: unknown},
     required: A,
     optional: B
 ): ParseQueryResult<A, B> {
- 
+    const requiredMapped = Object.entries(required).map(queryTemplateMapper(false)); 
+    const optionalMapped = Object.entries(required).map(queryTemplateMapper(true)); 
+
+    const templateParams = [...requiredMapped, ...optionalMapped];
+
+    for (const templateParam of templateParams) {
+        const exists = Object.hasOwn(query, templateParam.name);
+        if (!templateParam.isOptional) {
+            assert(exists);
+        }
+
+        const value = query[templateParam.name];
+
+        switch (templateParam.type) {
+            case QueryType.Integer:
+                parseInteger(value);
+                break;
+        
+            default:
+                break;
+        }
+    }
 
     // FOR TESTING
     return "" as unknown as any;
 }
 
+function parseInteger(value: unknown) {
+    const num = Number(value);
+    const isNumeric = !isNaN(num);
+    assert(isNumeric);
+    
+    const isInteger = Number.isInteger(num);
+    assert(isInteger);
+
+    return num;
+}
+
+function queryTemplateMapper(isOptional: boolean) {
+    return function([name, type]: [string, keyof IQueryType]) {
+        return { name, type, isOptional }   
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -11,33 +11,35 @@ import { getCurrentDayIndex, parseDate } from "./utils";
 import { Database } from "./database/db";
 import { DatabaseSetup } from "./database/dbSetup";
 import assert from "assert";
+import { parseQuery, QueryType } from "./api/utils";
 
 export let currentMenu: WeekMenu;
-let foodArchive: Database;
+// let foodArchive: Database;
 
-/* Ininitialise database*/
-const dbSetup = new DatabaseSetup("SafkaBot2", "mongodb://127.0.0.1:27017");
+// /* Ininitialise database*/
+// const dbSetup = new DatabaseSetup("SafkaBot2", "mongodb://127.0.0.1:27017");
 
-(async function(){
-    await dbSetup.newClient();
-    const db = dbSetup.getDatabase();
+// (async function(){
+//     await dbSetup.newClient();
+//     const db = dbSetup.getDatabase();
 
-    assert(db, new Error("Database undefined"));
+//     assert(db, new Error("Database undefined"));
 
-    foodArchive = new Database(db);
+//     foodArchive = new Database(db);
 
-    const poller = new Poller({ enableLogs: true });
-    poller.startPolling();
-    poller.on("polled", (menu) => {
-        currentMenu = menu;
+//     const poller = new Poller({ enableLogs: true });
+//     poller.startPolling();
+//     poller.on("polled", (menu) => {
+//         currentMenu = menu;
         
-        /* foodArchive menus */
-        foodArchive.weekMenu = currentMenu;
-        foodArchive.dayMenu = currentMenu.days[getCurrentDayIndex()];
-        /* Add current menu to mongoDb */
-        foodArchive.saveMenus();
-    });
-})()
+//         /* foodArchive menus */
+//         foodArchive.weekMenu = currentMenu;
+//         foodArchive.dayMenu = currentMenu.days[getCurrentDayIndex()];
+//         /* Add current menu to mongoDb */
+//         foodArchive.saveMenus();
+//     });
+// })()
+console.log("hello")
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -45,17 +47,13 @@ const app = express();
 app.use(cors());
 
 app.get("/api/v1/safka/", (req, res) => {
-    if (typeof req.query?.date != "string") return;
-    const date = parseDate(req.query.date);
 
-    if (!date) return;
-
-    console.log(date.toLocaleDateString());
+    const query = parseQuery(req.query, {test: QueryType.Integer}, {});
+    query.test
+    console.log("run")
 
 
-    if (currentMenu) {
-        res.json(currentMenu);
-    }
+    res.send("hello")
 });
 
 app.listen(PORT);
