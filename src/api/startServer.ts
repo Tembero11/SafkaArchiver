53a19d3451e5825bfa724c10530f737d666f8d57
jkop <jkop@kukkula.local>
2022-12-05 15:06:52 +0200

    Better api

--- src/api/startServer.ts
+++ src/api/startServer.ts
@@ -2,7 +2,9 @@ import cors from "cors";
 import express, { Router } from "express";
 import { getDayFromWeek } from "../foodUtils";
 import { currentMenu } from "../index";
+import { Weekday } from "../types";
 import { getCurrentDayIndex } from "../utils";
+import { apiResponse } from "./apiResponse";
 
 export const app = express();
 
@@ -12,22 +14,31 @@ const api = Router();
 api.use(cors());
 
 api.get("/v1/menu", (req, res) => {
-  const format = req.query.format == "true";
-
-  const result = format ? JSON.stringify(currentMenu, null, 2) : JSON.stringify(currentMenu);
-
-  res.status(200).type("application/json").end(result);
+  apiResponse(res, 200, { ...currentMenu });
 });
 
 api.get("/v1/menu/today", (req, res) => {
-  const format = req.query.format == "true";
-
   const today = getDayFromWeek(currentMenu, getCurrentDayIndex());
 
-  const result = format ? JSON.stringify(today, null, 2) : JSON.stringify(today);
-
-  res.status(200).type("application/json").end(result);
+  apiResponse(res, 200, { ...today });
 });
+
+
+api.get("/v1/menu/:dayId", (req, res) => {
+  const dayId = +req.params.dayId;
+
+  if (Object.hasOwn(Weekday, dayId)) {
+    const day = getDayFromWeek(currentMenu, dayId);
+    apiResponse(res, 200, { ...day });
+  } else {
+    apiResponse(res, 400, { msg: "Invalid dayId" })
+  }
+});
+
+app.get("*", function (req, res) {
+  apiResponse(res, 404);
+});
+
 
 interface StartServerOptions {
   apiBaseRoute?: string
@@ -35,5 +46,5 @@ interface StartServerOptions {
 
 export function startServer(port: number, options?: StartServerOptions) {
   app.use(options?.apiBaseRoute || "/api", api);
-  app.listen(port); 
+  app.listen(port);
 }
